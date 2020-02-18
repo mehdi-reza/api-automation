2c22b19c28f40c691643f077b52af252a533b5f3
Mehdi Raza <mehdi@venturedive.com>
2020-02-18 13:54:41 +0500

    PreProcess.andThen for chaining multiple processors

--- api-automation/src/main/java/vend/api/automation/PreProcessor.java
+++ api-automation/src/main/java/vend/api/automation/PreProcessor.java
@@ -4,5 +4,10 @@ import com.google.gson.JsonObject;
 
 @FunctionalInterface
 public interface PreProcessor {
+	
 	public void process(JsonObject data);
+	
+	default public PreProcessor andThen(PreProcessor after) {
+		return data -> { process(data); after.process(data);};
+	}
 }
